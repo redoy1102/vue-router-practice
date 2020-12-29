59eb4ab989d4a68417d00e0dd15ef0c77954da42
Md Fuadul Islam Redoy <mdfuadulislamhridoy52@gmail.com>
2020-12-30 00:31:13 +0600

    Bootstrap added

---
+++ src/components/Customer/CustomerDetail.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CustomerDetail"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Customer/CustomerStart.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "CustomerStart"
+}
+</script>
+
+<style scoped>
+
+</style>
